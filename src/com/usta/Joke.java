8a0cfc07d7d7056e0c1b4fca4e5df377a4408d50
jxd <jxd@8f0a1f53-778c-5a46-8998-27fa251dfd5d>
2014-03-03 23:22:16 +0000

    joke本地数据库修改了
    git-svn-id: https://59.78.93.208/svn/AndroidApp@5 8f0a1f53-778c-5a46-8998-27fa251dfd5d

--- src/com/usta/Joke.java
+++ src/com/usta/Joke.java
@@ -58,7 +58,7 @@ public class Joke extends Activity implements OnClickListener {
 		db.execSQL("insert into jokes (text) values ('丑女跟和尚同船渡河，和尚无意间瞅了丑女一眼，丑女立刻大发脾气：大胆秃头，光天化日之下竟敢偷看良家妇女！ ’ 和尚一听，吓得连忙把眼睛闭上。丑女一见，更生气了：你偷看我还不算，还敢闭上眼睛在心里想我！  和尚无法跟她讲道理，又把脸扭到一边。丑女得理不饶人，双手叉腰，大声训斥道：你觉得无脸见我，正好说明你心中有鬼！')");
 		db.execSQL("insert into jokes (text) values ('女A：你猜猜我现在多重？女B：我估计没有一百斤……女A：你真会说话。女B：也有一百二十斤。')");
 		db.execSQL("insert into jokes (text) values ('我前面一女生平胸，然后我问她哪天你晚上自己回家，被劫色怎么办…？她淡淡的回了句：‘我就脱了上衣，然后说，别激动，是自己人’…自己人…己人…人…')");db.execSQL("insert into jokes (text) values ('我前面一女生平胸，然后我问她哪天你晚上自己回家，被劫色怎么办…？她淡淡的回了句：‘我就脱了上衣，然后说，别激动，是自己人’…自己人…己人…人…')");
-		db.close();
+		//db.close();
 		Cursor cursor =db.rawQuery("select * from jokes", null);
 		if(cursor.moveToLast())
 		{
