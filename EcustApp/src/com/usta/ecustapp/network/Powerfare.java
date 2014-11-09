f65131c0e7c3e739943ba272c4392b6d47cf184f
ccn <ccn@8f0a1f53-778c-5a46-8998-27fa251dfd5d>
2014-11-09 12:56:17 +0000

    remove unused package
    git-svn-id: https://59.78.93.208/svn/AndroidApp@133 8f0a1f53-778c-5a46-8998-27fa251dfd5d

--- EcustApp/src/com/usta/ecustapp/network/Powerfare.java
+++
@@ -1,8 +0,0 @@
-package com.usta.ecustapp.network;
-
-public class Powerfare {
-	public static String getPowerFare(String building, String room) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}
--- EcustApp/src/com/usta/ecustapp/service/NewsService.java
+++ EcustApp/src/com/usta/ecustapp/service/NewsService.java
@@ -1,4 +1,4 @@
-package com.usta.ecustapp.network;
+package com.usta.ecustapp.service;
 
 import java.net.URI;
 
@@ -13,25 +13,25 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class News {
+public class NewsService {
 
-	public static JSONArray getNewsTitles( )
-	{
-		String url="http://59.78.93.208:9092/Newstitles";
+	public JSONArray getNewsTitles() {
+		String url = "http://59.78.93.208:9092/Newstitles";
 		HttpClient client = new DefaultHttpClient();
-		 client.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 3000);
-		    client.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 3000 );
-			
+		client.getParams().setParameter(
+				CoreConnectionPNames.CONNECTION_TIMEOUT, 3000);
+		client.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 3000);
+
 		HttpGet request;
 		try {
 			request = new HttpGet(new URI(url));
 			HttpResponse response = client.execute(request);
-			
+
 			if (response.getStatusLine().getStatusCode() == 200) {
 				HttpEntity entity = response.getEntity();
 				if (entity != null) {
 					String out = EntityUtils.toString(entity);
-					JSONArray jsonArray=new JSONArray(out);
+					JSONArray jsonArray = new JSONArray(out);
 					return jsonArray;
 				}
 			}
@@ -42,26 +42,25 @@ public class News {
 		}
 		return null;
 	}
-	
-	
-	public static JSONObject getNewsDetails(int id)
-	{
-		String url="http://59.78.93.208:9092/Newsdetails?id="+id+"";
+
+	public JSONObject getNewsDetails(int id) {
+		String url = "http://59.78.93.208:9092/Newsdetails?id=" + id + "";
 		HttpClient client = new DefaultHttpClient();
-		 client.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 3000);
-		    client.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 3000 );
-			
+		client.getParams().setParameter(
+				CoreConnectionPNames.CONNECTION_TIMEOUT, 3000);
+		client.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 3000);
+
 		HttpPost request;
 		try {
 			request = new HttpPost(new URI(url));
 			HttpResponse response = client.execute(request);
-			
+
 			if (response.getStatusLine().getStatusCode() == 200) {
 				HttpEntity entity = response.getEntity();
 				if (entity != null) {
 					String out = EntityUtils.toString(entity);
-					JSONArray jsonArray=new JSONArray(out);
-					
+					JSONArray jsonArray = new JSONArray(out);
+
 					return jsonArray.optJSONObject(0);
 				}
 			}
@@ -72,5 +71,5 @@ public class News {
 		}
 		return null;
 	}
-	
+
 }
